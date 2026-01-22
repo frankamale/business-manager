e5fd9fa5d10056b19dfe429109986d18d2c2f553
frankamale <frankamale5@gmail.com>
2026-01-22 12:25:44 +0300

    skeletonise

--- lib/bac_monitor/lib/components/dashboard/kpi_overview.dart
+++ lib/bac_monitor/lib/components/dashboard/kpi_overview.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 import '../../controllers/mon_kpi_overview_controller.dart';
 import '../../widgets/dashboard/kpi_card.dart';
 
@@ -34,54 +35,47 @@ class _KpiOverviewSectionState extends State<KpiOverviewSection> {
         );
       }
 
-      if (controller.hasError.value) {
-        return const SizedBox(
-          height: 200,
-          child: Center(
-            child: Text(
-              'Error loading KPI data',
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-        );
-      }
+      final isLoading = controller.isLoading.value;
 
-      return GridView.count(
-        padding: EdgeInsets.zero,
-        crossAxisCount: 2,
-        crossAxisSpacing: 12,
-        mainAxisSpacing: 12,
-        shrinkWrap: true,
-        physics: const NeverScrollableScrollPhysics(),
-        childAspectRatio: 1.3,
-        children: [
-          KpiCard(
-            title: "Total Sales",
-            value: controller.totalSales.value,
-            trendValue: controller.salesTrend.value,
-            trendDirection: controller.salesTrendDirection.value,
-            unit: controller.unit.value,
-          ),
-          KpiCard(
-            title: "Avg. Basket Size",
-            value: controller.avgBasketSize.value,
-            trendValue: controller.basketTrend.value,
-            trendDirection: controller.basketTrendDirection.value,
-            unit: controller.unit.value,
-          ),
-          KpiCard(
-            title: "Total Transactions",
-            value: controller.totalTransactions.value,
-            trendValue: controller.transactionsTrend.value,
-            trendDirection: controller.transactionsTrendDirection.value,
-          ),
-          KpiCard(
-            title: "Active / Total Stores",
-            value: controller.activeTotalStores.value,
-            trendValue: controller.storesTrend.value,
-            trendDirection: controller.storesTrendDirection.value,
-          ),
-        ],
+      return Skeletonizer(
+        enabled: isLoading,
+        child: GridView.count(
+          padding: EdgeInsets.zero,
+          crossAxisCount: 2,
+          crossAxisSpacing: 12,
+          mainAxisSpacing: 12,
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          childAspectRatio: 1.3,
+          children: [
+            KpiCard(
+              title: "Total Sales",
+              value: controller.totalSales.value,
+              trendValue: controller.salesTrend.value,
+              trendDirection: controller.salesTrendDirection.value,
+              unit: controller.unit.value,
+            ),
+            KpiCard(
+              title: "Avg. Basket Size",
+              value: controller.avgBasketSize.value,
+              trendValue: controller.basketTrend.value,
+              trendDirection: controller.basketTrendDirection.value,
+              unit: controller.unit.value,
+            ),
+            KpiCard(
+              title: "Total Transactions",
+              value: controller.totalTransactions.value,
+              trendValue: controller.transactionsTrend.value,
+              trendDirection: controller.transactionsTrendDirection.value,
+            ),
+            KpiCard(
+              title: "Active / Total Stores",
+              value: controller.activeTotalStores.value,
+              trendValue: controller.storesTrend.value,
+              trendDirection: controller.storesTrendDirection.value,
+            ),
+          ],
+        ),
       );
     });
   }
